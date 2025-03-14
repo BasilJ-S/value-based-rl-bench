9b2ed5eb6db02396d4c25e980d2effe827e58504
BasilJ-S <18bjjs@queensu.ca>
2025-03-14 14:19:38 -0400

    cleaning the plots

--- agents.py
+++ agents.py
@@ -1,9 +1,11 @@
 
-import re
+from hmac import new
+from tokenize import Double
 import torch
 import numpy as np
 import gymnasium as gym
 import matplotlib.pyplot as plt
+import torch.optim.rmsprop
 from tqdm import tqdm
 
 
@@ -33,18 +35,21 @@ class DQN:
         self.reinit_weights()
         
         self.Q.to(device)
-        self.optimizer = torch.optim.AdamW(self.Q.parameters(), lr=lr)
+        self.optimizer = torch.optim.RMSprop(self.Q.parameters(), lr=lr)
     
+    # Batch is a list of tuples from the replay buffer
+    # All will be numpy arrays
     def update(self, batch) -> None:
         # Batch is a list of tuples from the replay buffer
         # Update the Q function
         # Every element 0 of the tuple is the observation. We need to stack them to get a tensor of observations
         # Format is (observation, action, reward, observation_prime, terminated or truncated)
-        states = torch.stack([torch.Tensor(b[0]) for b in batch])
-        actions = torch.tensor([b[1] for b in batch])
-        rewards = torch.tensor([b[2] for b in batch])
-        next_states = torch.stack([torch.Tensor(b[3]) for b in batch])
-        terminated = torch.tensor([b[4] for b in batch], dtype=torch.bool)
+        batch = list(zip(*batch))
+        states = torch.stack([torch.from_numpy(s).float() for s in batch[0]])
+        actions = torch.tensor(batch[1])
+        rewards = torch.tensor(batch[2])
+        next_states = torch.stack([torch.from_numpy(s).float() for s in batch[3]])
+        terminated = torch.tensor(batch[4], dtype=torch.bool)
         #print(terminated)
 
         # Get the next values
@@ -52,7 +57,7 @@ class DQN:
             next_values = torch.max(self.Q.forward(next_states), dim=1).values
 
         # Use terminated to mask next_values
-        next_values = next_values.masked_fill(terminated, 0)
+        next_values[terminated] =0 
         y = rewards + self.gamma * next_values
 
         value_estimates = self.Q.forward(states)
@@ -95,11 +100,13 @@ class DQN:
         #Plot the moving average
         moving_average = []
         for i in range(len(rewards)):
-            moving_average.append(np.mean(rewards[max(0,i-10):min(len(rewards),i+10)]))
+            moving_average.append(np.mean(rewards[max(0,i-5):min(len(rewards),i+5)]))
         axs[0,1].plot(moving_average)
         axs[0,1].set_title("Moving Average")
         axs[0,1].set_xlabel("Episode")
         axs[0,1].set_ylabel("Reward")
+
+
         #Space out the subplots by a bit
         plt.tight_layout()
         plt.show()
@@ -109,6 +116,12 @@ class DQN:
             if isinstance(layer, torch.nn.Linear):
                 torch.nn.init.uniform_(layer.weight, -0.001, 0.001)
                 torch.nn.init.uniform_(layer.bias, -0.001, 0.001)
+
+    def select_action(self, observation: torch.Tensor) -> int:
+        # Take in observation from env, return action
+        with torch.no_grad():
+            estimates = self.Q.forward(observation)
+        return torch.argmax(estimates).item()
 
 
     def train(self, num_episodes: int, episode_len: int, epsilon: float = 0.3, use_buffer = False, 
@@ -124,23 +137,28 @@ class DQN:
         rewards = []
 
         for episode in tqdm(range(num_episodes)):
+            if False and episode == num_episodes - 1:
+                new_env = gym.make(self.env.spec.id, render_mode='human')
+                self.env.close()
+                self.env = new_env
+            else:
+                #Don't render the other episodes
+                pass
+            episode_rewards = []
             observation, _ = self.env.reset(seed=self.seed + episode) # ADD epsiode so the seed is different for each episode
             #print(type(observation))
             #print(observation.shape)
             for t in range(episode_len):
                 rand = np.random.rand() 
                 if rand < epsilon:
-                    action = torch.randint(self.env.action_space.n, (1,))
+                    action = np.random.randint(0,self.env.action_space.n)
                 else:
-                    with torch.no_grad():
-                        estimates = self.Q.forward(torch.from_numpy(observation).float().to(self.device))
-                    #print(f"TIME: {t}, Episode: {episode}, estimates: ", estimates)
-                        action = torch.argmax(estimates).item() 
+                    action = self.select_action(torch.from_numpy(observation).float().to(self.device))
                 
                 #print("Action off device: ", action)
                 observation_prime, reward, terminated, truncated, info = self.env.step(action)                    
 
-                rewards.append(reward)
+                episode_rewards.append(reward)
                 
                 # Replay buffer
                 if use_buffer:
@@ -167,6 +185,7 @@ class DQN:
                 observation = observation_prime
                 if terminated or truncated:
                     break
+            rewards.append(sum(episode_rewards))
 
         self.env.close()
         if plot_results:
